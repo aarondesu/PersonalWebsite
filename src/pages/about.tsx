9c96905d1a663a0c32a66a2111292d9962dcd5e8
Shinudesu <arokidaaron93@gmail.com>
2024-03-31 11:19:51 +0800

    refactor: changed about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,40 +4,73 @@ import React from 'react';
 const AboutPage: React.FC = () => {
   return (
     <motion.div
-      className="flex flex-col md:flex-row lg:m-auto mx-auto mt-10 font-Roboto md:space-x-10"
+      className="flex flex-col md:flex-row lg:m-auto mx-auto font-Roboto md:space-x-10"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="md:max-w-lg max-w-80 space-y-4  text-center">
-        <div className="text-5xl font-black mb-10">&#123; About &#125;</div>
-        <div className="text-2xl font-black text-gray-600">
-          Hey there! I'm Aaron
+      <div className="space-y-4">
+        <div className="text-5xl font-black mb-10 text-center">
+          &#123; About &#125;
         </div>
-        <p>
-          A self-taught web developer/software engineer, with dreams as big as
-          my love for technology and programing.
-        </p>
-        <p>
-          Passion drives me. Whether it's coding up a storm or diving into the
-          latest web dev trends, I'm all about turning ideas into interactive
-          online experiences.
-        </p>
-        <p>
-          I'm not just a code junkie; I'm a perpetual learner. From tinkering
-          with HTML and CSS to mastering JavaScript wonders like React and
-          Node.js, I'm on a rollercoaster of continuous improvement. As
-          throughout the years, I've been pushing myself to learn and improve
-          myself whenever I have free time on technologies that interest me,
-          whether it be Web Development, Software Engineering, and even Game
-          Development!
-        </p>
+        <div className="flex md:flex-row flex-col md:space-x-10 md:space-y-0 space-y-10 md:divide-y-0 divide-y-2 divide-dotted">
+          <div className="md:max-w-lg space-y-4">
+            <h1 className="text-2xl font-black ">Hey there! I'm Aaron</h1>
+            <p>
+              A self-taught web developer/software engineer, with dreams as big
+              as my love for technology and programing.
+            </p>
+            <p>
+              Passion drives me. Whether it's coding up a storm or diving into
+              the latest web dev trends, I'm all about turning ideas into
+              interactive online experiences.
+            </p>
+            <p>
+              I'm not just a code junkie; I'm a perpetual learner. From
+              tinkering with HTML and CSS to mastering JavaScript wonders like
+              React and Node.js, I'm on a rollercoaster of continuous
+              improvement. As throughout the years, I've been pushing myself to
+              learn and improve myself whenever I have free time on technologies
+              that interest me, whether it be Web Development, Software
+              Engineering, and even Game Development!
+            </p>
 
-        <p>
-          With that said, I am aspiring to work on myself and work on my career
-          after the numerous challenges I've had the in the past, and I try to
-          look forward to the future as much as I can.
-        </p>
+            <p>
+              With that said, I am aspiring to work on myself and work on my
+              career after the numerous challenges I've had the in the past, and
+              I try to look forward to the future as much as I can.
+            </p>
+          </div>
+
+          <div className="space-y-4 md:py-0 py-10">
+            <h1 className="text-2xl font-black">Skills</h1>
+            <div className="space-y-2">
+              <h1 className="font-bold">Techincal Skills</h1>
+              <ul className="space-y-2 list-inside list-disc">
+                <li>C#, Java, and Javascript/Typescript</li>
+                <li>Node.js</li>
+                <li>Frontend with React/ReduxJs/NextJs</li>
+                <li>Backend with ExpressJs/NestJs</li>
+                <li>Database management with MySQL/PostgresSQL</li>
+              </ul>
+            </div>
+            <div className="space-y-2">
+              <h1 className="font-bold">Tools</h1>
+              <ul className="space-y-2 list-inside list-disc">
+                <li>Krita</li>
+                <li>Figma</li>
+                <li>Visual Studio Code</li>
+                <li>Github</li>
+              </ul>
+            </div>
+            <div className="space-y-2">
+              <h1 className="font-bold">Development Practices</h1>
+              <ul className="space-y-2 list-inside list-disc">
+                <li>Agile Development</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </motion.div>
   );
--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -11,61 +11,57 @@ const ErrorPage: React.FC = () => {
   const navigate = useNavigate();
   const error = useRouteError();
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <motion.div className="flex h-screen" initial={{}} animate={{}} exit={{}}>
-        <div className="text-center font-Roboto m-auto">
-          <div className="md:text-9xl text-8xl font-bold">
-            &#123; {error.status} &#125;
+  return (
+    <motion.div className="flex h-screen" initial={{}} animate={{}} exit={{}}>
+      <div className="text-center font-Roboto m-auto space-y-6">
+        {isRouteErrorResponse(error) ? (
+          <div>
+            <div className="md:text-9xl text-8xl font-bold">
+              &#123; {error.status} &#125;
+            </div>
+            <div className="text-gray-400 md:text-2xl text-1xl">
+              {error.statusText}
+            </div>
+            <div className="text-gray-400 md:text-2xl text-1xl">
+              {error.data?.message}
+            </div>
           </div>
-          <div className="text-gray-400 md:text-2xl text-1xl">
-            {error.statusText}
+        ) : (
+          <div>
+            <h1 className="md:text-9xl text-8xl font-bold">Oops</h1>
           </div>
-          <div className="text-gray-400 md:text-2xl text-1xl">
-            {error.data?.message}
-          </div>
-          <div className="flex space-x-4 justify-center mt-6">
-            <Link
-              to={'..'}
-              onClick={(e) => {
-                e.preventDefault();
-                navigate(-1);
-              }}
-              className="underline"
-            >
-              Go Back
-            </Link>
-            <Link to={'/'} className="underline">
-              Go Home
-            </Link>
-          </div>
+        )}
+        <div className="flex space-x-4 justify-center mt-6">
+          <Link
+            to={'..'}
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
+            className="underline"
+          >
+            Go Back
+          </Link>
+          <Link to={'/'} className="underline">
+            Go Home
+          </Link>
         </div>
-      </motion.div>
-    );
-  } else {
-    return (
-      <div className="flex h-screen">
-        <div className="flex flex-col m-auto font-Roboto">
-          <h1 className="text-9xl font-black">&#123; Oops &#125;</h1>
-          <div className="flex space-x-4 justify-center mt-6">
-            <Link
-              to={'..'}
-              onClick={(e) => {
-                e.preventDefault();
-                navigate(-1);
-              }}
+
+        <div>
+          <span>
+            If any issue is found, please report it to{' '}
+            <a
+              href="https://github.com/aarondesu/PersonalWebsite/issues"
               className="underline"
             >
-              Go Back
-            </Link>
-            <Link to={'/'} className="underline">
-              Go Home
-            </Link>
-          </div>
+              issues page
+            </a>
+            .
+          </span>
         </div>
       </div>
-    );
-  }
+    </motion.div>
+  );
 };
 
 export default ErrorPage;
